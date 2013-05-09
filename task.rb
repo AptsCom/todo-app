62e911893fa1de483ca34d1bfd5905021ff2d4fe
Kunal Bhatt <bhattk09@gmail.com>
2013-05-09 11:07:18 -0500

    Initial commit

---
+++ task.rb
@@ -0,0 +1,17 @@
+class Task
+  attr_accessor :complete
+  attr_reader :description
+ 
+  def initialize(description)
+    @description = description
+    @complete = false
+  end
+ 
+  def complete?
+    @complete
+  end
+ 
+  def complete!
+    @complete = true
+  end
+end
